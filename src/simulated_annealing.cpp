06fadf60eb9cf25a072a2c74dabbd2d77d5c0426
Fábio Gaspar <fabiodrg666@users.noreply.github.com>
2019-05-26 01:56:55 +0100

    Completed simulated annealing (#9)

--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-Timetable* _get_next_state(Timetable* tt, Instance& inst) {
+Timetable* _get_next_state(Timetable* tt) {
 	// generate a random days and slots
 	uint8_t day_a, slot_a, day_b, slot_b;
 
@@ -51,52 +51,55 @@ double cooling_schedule_A(annealing_info info, uint current_iteration) {
 	return info.initial_temperature * pow((1 / info.initial_temperature), (double)current_iteration / info.iterations);
 }
 
+double cooling_schedule_B(annealing_info info, uint current_iteration) {
+	double A = ((info.initial_temperature - 1) * (info.iterations + 1)) / info.iterations;
+	double B = info.initial_temperature - A;
+	printf("A: %.3lf | B: %.3lf | R: %.3lf\n", A, B, A / (current_iteration + 1) + B);
+	return A / (current_iteration + 1) + B;
+}
+
 Timetable* simulated_annealing(Instance& inst, Timetable* (*generate_initial_state)(Instance&), annealing_info info) {
 	// generate initial solution
 	Timetable* tt = generate_initial_state(inst);
 
 	// calculate the score for this random solution
 	int current_score = tt->calculateScore();
-	int iteration = 0;
 	double temperature = info.initial_temperature;
 	for (int i = info.iterations; i > 0; i--) {
-		for (int j = 0; j < 10; j++) {
-			// generate possible next state
-			Timetable* next_tt = _get_next_state(tt, inst);
-			if (next_tt == nullptr) continue;
-			int next_score = next_tt->calculateScore();
-			int delta_score = next_score - current_score;
 
-			// evaluate the new score and accept or discard the new state
-			if (delta_score < 0) {
-				// it is an enhancent, accept it
-				cout << "Delta: " << delta_score;
+		// generate possible next state
+		Timetable* next_tt = _get_next_state(tt);
+		if (next_tt == nullptr) continue;
+		int next_score = next_tt->calculateScore();
+		int delta_score = next_score - current_score;
+
+		// evaluate the new score and accept or discard the new state
+		if (delta_score < 0) {
+			// it is an enhancent, accept it
+			current_score = next_score;
+			delete tt;
+			tt = next_tt;
+		} else {
+			// calculate the probability
+			double probability = exp(-delta_score / temperature);
+			// random probability acceptance
+			double r = (double)rand() / RAND_MAX;
+			//printf("Delta: %d | P: %.3lf | E: %.3lf\n", delta_score, probability, r);
+
+			if (probability > r) {
 				current_score = next_score;
 				delete tt;
 				tt = next_tt;
 			} else {
-				double probability = exp(-delta_score / temperature);
-				double r = (double)rand() / RAND_MAX;
-				cout << "Delta: " << delta_score << " | P: " << probability << " | E: " << r << endl;
-				if (probability > r) {
-					// this is a bad score, but given the probability, let's accept it
-					//cout << "Bad score, but accept it" << endl;
-					current_score = next_score;
-					delete tt;
-					tt = next_tt;
-				} else {
-					//cout << "Bad score, reject it" << endl;
-					// drop the generated next state
-					delete next_tt;
-				}
+				delete next_tt;
 			}
-
-			cout << "\tNew score : " << next_score << endl;
 		}
 
+		cout << "New score : " << next_score << endl;
+
 		// decrease the temperature
-		temperature = cooling_schedule_A(info, iteration);
-		cout << temperature << endl;
+		temperature = info.cooling_schedule(info, info.iterations - i);
+		cout << "Temperature : " << temperature << endl;
 	}
 	return tt;
 }
